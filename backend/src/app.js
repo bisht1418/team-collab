1e3609bbcdd15f7c6edc4fe71a7f9c9fc6f171cb
bisht1418 <bishtnrj1418@gmail.com>
2025-04-24 02:11:54 +0530

    add the specific origin name

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,11 +9,18 @@ const { errorConverter, errorHandler } = require('./middlewares/error.middleware
 
 const app = express();
 
-app.use(cors({
-  origin: true,
+const corsOptions = {
+  origin: [
+    'https://team-collab-7mfw.vercel.app',
+    'http://localhost:3001'  
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   credentials: true,
-}));
-app.options('*', cors());
+  allowedHeaders: ['Content-Type', 'Authorization']
+};
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
