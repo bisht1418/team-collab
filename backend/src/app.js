85c826c88e03f89d49e8a6c12e9d82bb1bde491e
bisht1418 <bishtnrj1418@gmail.com>
2025-04-24 02:22:46 +0530

    add corse 01

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,17 +10,25 @@ const { errorConverter, errorHandler } = require('./middlewares/error.middleware
 const app = express();
 
 const corsOptions = {
-  origin: [
-    'https://team-collab-7mfw.vercel.app',
-    'http://localhost:3001'  
-  ],
+  origin: function(origin, callback) {
+    const allowedOrigins = [
+      'https://team-collab-7mfw.vercel.app',
+      'http://localhost:3001',
+    ];
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   credentials: true,
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
 };
 
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
+
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
